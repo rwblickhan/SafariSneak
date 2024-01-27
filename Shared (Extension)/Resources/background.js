17355dd4f678be65f8ae546501d38c328e077867
Russell Blickhan <rwblickhan@gmail.com>
2024-01-26 21:59:01 -0800

    Start the project out

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,6 +1,10 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+async function handleOpenLink(request, sender, sendResponse) {
+  if (request.id === "open_link" && request.url) {
+    browser.tabs.create({ url: request.url });
+    await browser.tabs.create({ url });
+  } else {
+    console.error(`Invalid message received from content script: ${request}`);
+  }
+}
 
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
-});
+browser.runtime.onMessage.addListener(handleOpenLink);
--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,46 @@
+console.log("Hello from content.js!");
+let isListening = false;
+let shouldOpenInNewTab = false;
+const links = getAllLinks();
+
+async function handleSelection(key) {
+  if (shouldOpenInNewTab) {
+    await browser.runtime.sendMessage({ id: "open_link", url: links[key] });
+  } else {
+    window.location.href = links[key];
+  }
+  isListening = false;
+  shouldOpenInNewTab = false;
+}
+
+function getAllLinks() {
+  const links = document.querySelectorAll("a");
+  let linksUrls = [];
+  for (const link of links) {
+    linksUrls.push(link.href);
+  }
+  return linksUrls;
+}
+console.log(JSON.stringify(links));
+
+document.addEventListener("keydown", async function (event) {
+  if (isListening) {
+    await handleSelection(event.key);
+    return;
+  }
+
+  if (event.key === "s" || event.key === "S") {
+    console.log("The 's' key was pressed.");
+    isListening = true;
+    shouldOpenInNewTab = event.shiftKey;
+    console.log(`Should open in new tab: ${shouldOpenInNewTab}`);
+  }
+});
+
 browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+  console.log("Received response: ", response);
 });
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+  console.log("Received request: ", request);
 });
